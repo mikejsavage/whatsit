ce1bdfae5ebb94e5280828e47279b94e1c843021
Michael Savage <mikejsavage@gmail.com>
2015-04-08 20:33:05 +0100

    Only connect to WhatsApp when a client connects to our domain socket

--- src/layer.py
+++ src/layer.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import sys, os, json, threading, socket
+import sys, os, json, logging, threading
 
 import config
 
@@ -10,57 +10,56 @@ from yowsup.layers.protocol_messages.protocolentities  import BroadcastTextMessa
 from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
 from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
 
-# This class creates the input pipe and spawns a thread to read it
-# Each line we read is parsed as JSON and treated as a message
-class SocketReader( object ):
+logging.basicConfig( level = logging.INFO )
+logger = logging.getLogger( __name__ )
+
+class DomainSocketLayer( YowInterfaceLayer ):
     def __init__( self ):
-        addr = "/tmp/whatsit_%s.sock" % config.credentials[ "phone" ]
-        try:
-            os.unlink( addr )
-        except OSError:
-            if os.path.exists( addr ):
-                raise
-
-        self.sock = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
-        self.sock.bind( addr )
-        self.sock.listen( 1 )
-
-        self.th = threading.Thread( target = self.accept )
+        self.th = threading.Thread( target = self.readSocket )
         self.th.daemon = True
         self.th.start()
 
-    def accept( self ):
-        # Loop forever incase the other side closes the pipe
-        while True:
-            self.client, _ = self.sock.accept()
-            fd = self.client.makefile()
-
-            for line in iter( fd.readline, "" ):
-                try:
-                    cmd = json.loads( line )
-                    outgoing = None
-
-                    if cmd[ "action" ] == "message":
-                        outgoing = TextMessageProtocolEntity( cmd[ "body" ], to = cmd[ "to" ] )
-                    elif cmd[ "action" ] == "broadcast":
-                        outgoing = BroadcastTextMessage( cmd[ "to" ], cmd[ "body" ] )
-
-                    if outgoing:
-                        self.toLower( outgoing )
-                except:
-                    print( "Bad line:", line, file = sys.stderr )
-
-            self.client.close()
-            self.client = None
-
-class EchoLayer( SocketReader, YowInterfaceLayer ):
-    def __init__( self ):
-        # Start the input thread
-        super( EchoLayer, self ).__init__()
         YowInterfaceLayer.__init__( self )
 
-        self.client = None
+    # Read messages from the socket and forward them to WhatsApp
+    def readSocket( self ):
+        for line in iter( self.fd.readline, "" ):
+            logger.debug( "Received line: %s", line )
 
+            try:
+                cmd = json.loads( line )
+                outgoing = None
+
+                if cmd[ "action" ] == "message":
+                    outgoing = TextMessageProtocolEntity( cmd[ "body" ], to = cmd[ "to" ] )
+                elif cmd[ "action" ] == "broadcast":
+                    outgoing = BroadcastTextMessage( cmd[ "to" ], cmd[ "body" ] )
+
+                if outgoing:
+                    self.toLower( outgoing )
+            except:
+                logger.error( "Couldn't parse input line! Maybe you're missing the \"action\" field." )
+                logger.error( line )
+
+        logger.info( "Local client disconnected. Disconnecting from WhatsApp" )
+        self.disconnect()
+
+    # We received a nice message
+    def onText( self, msg ):
+        logger.debug( "Received message from %s: %s", msg.getFrom(), msg.getBody() )
+
+        # Write JSON to the socket
+        self.fd.write( json.dumps( {
+            "from" : msg.getFrom(),
+            "body" : msg.getBody(),
+        } ) + "\n" )
+        self.fd.flush()
+
+        # Acknowledge it
+        receipt = OutgoingReceiptProtocolEntity( msg.getId(), msg.getFrom() )
+        self.toLower( receipt )
+
+    # Check it's a message we care about, then pass it to self.onText
     @ProtocolEntityCallback( "message" )
     def onMessage( self, messageProtocolEntity ):
         if not messageProtocolEntity.isGroupMessage():
@@ -72,14 +71,3 @@ class EchoLayer( SocketReader, YowInterfaceLayer ):
     def onReceipt( self, receipt ):
         ack = OutgoingAckProtocolEntity( receipt.getId(), "receipt", "delivery" )
         self.toLower( ack )
-
-    def onText( self, msg ):
-        if self.client:
-            self.client.sendall( json.dumps( {
-                "from" : msg.getFrom(),
-                "body" : msg.getBody(),
-            } ) + "\n" )
-
-            # Acknowledge the incoming message
-            receipt = OutgoingReceiptProtocolEntity( msg.getId(), msg.getFrom() )
-            self.toLower( receipt )
--- src/main.py
+++ src/main.py
@@ -1,34 +1,60 @@
-import sys, os, yowsup
+import sys, os, socket, functools, logging
 
 import config
-from layer import EchoLayer
+from layer import DomainSocketLayer
 
-from yowsup.stacks          import YowStack, YowStackBuilder
-from yowsup.layers          import YowLayerEvent
-from yowsup.layers.auth     import AuthError
-from yowsup.layers.network  import YowNetworkLayer
+from yowsup.stacks           import YowStack, YowStackBuilder
+from yowsup.layers           import YowLayerEvent
+from yowsup.layers.auth      import AuthError
+from yowsup.layers.interface import YowInterfaceLayer
+from yowsup.layers.network   import YowNetworkLayer
+
+logging.basicConfig( level = logging.INFO )
+logger = logging.getLogger( __name__ )
 
 config.init( sys.argv[ 1 ] )
 
+addr = "/tmp/whatsit_%s.sock" % config.credentials[ "phone" ]
+logger.info( "Binding to %s", addr )
+
+sock = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
+sock.bind( addr )
+sock.listen( 1 )
+
 try:
-    credentials = (
-        config.credentials[ "phone" ],
-        config.credentials[ "password" ]
-    )
+    while True:
+        logger.info( "Waiting for a local client" )
+        client, _ = sock.accept()
+        fd = client.makefile()
+        logger.info( "Local client connected! Connecting to WhatsApp" )
 
-    stack = YowStackBuilder().pushDefaultLayers( True ).push( EchoLayer ).build()
-    stack.setCredentials( credentials )
-    stack.broadcastEvent( YowLayerEvent( YowNetworkLayer.EVENT_STATE_CONNECT ) )
+        # We want to use fd in our DomainSocketLayer, so make a shim class
+        class ProxyLayer( DomainSocketLayer, YowInterfaceLayer ):
+            def __init__( self ):
+                self.fd = fd
+                super( ProxyLayer, self ).__init__()
 
-    stack.loop()
+        credentials = (
+            config.credentials[ "phone" ],
+            config.credentials[ "password" ]
+        )
+
+        stack = (
+            YowStackBuilder()
+            .pushDefaultLayers( True )
+            .push( ProxyLayer )
+            .build()
+        )
+
+        stack.setCredentials( credentials )
+        stack.broadcastEvent( YowLayerEvent( YowNetworkLayer.EVENT_STATE_CONNECT ) )
+
+        stack.loop()
 
 except AuthError as e:
     print( "Authentication Error: " + e.message )
 
 except KeyboardInterrupt:
-    addr = "/tmp/whatsit_%s.sock" % config.credentials[ "phone" ]
     os.unlink( addr )
 
     sys.exit( 0 )
-
-print( "closed by myself" )
