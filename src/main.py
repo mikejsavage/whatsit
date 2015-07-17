4ffeae0f2aace488b0afc936204303e46d04a956
Michael Savage <mikejsavage@gmail.com>
2015-07-17 22:17:20 +0200

    Explicitly build the stack because the wiki example does and that fixes everything

--- src/main.py
+++ src/main.py
@@ -3,11 +3,19 @@ import sys, os, socket, functools, logging
 import config
 from layer import DomainSocketLayer
 
-from yowsup.stacks           import YowStack, YowStackBuilder
-from yowsup.layers           import YowLayerEvent
-from yowsup.layers.auth      import AuthError
-from yowsup.layers.interface import YowInterfaceLayer
-from yowsup.layers.network   import YowNetworkLayer
+from yowsup.layers.auth              import YowAuthenticationProtocolLayer, AuthError
+from yowsup.layers.interface         import YowInterfaceLayer
+from yowsup.layers.protocol_messages import YowMessagesProtocolLayer
+from yowsup.layers.protocol_receipts import YowReceiptProtocolLayer
+from yowsup.layers.protocol_acks     import YowAckProtocolLayer
+from yowsup.layers.network           import YowNetworkLayer
+from yowsup.layers.coder             import YowCoderLayer
+
+from yowsup.stacks import YowStack
+from yowsup.common import YowConstants
+from yowsup.layers import YowLayerEvent
+from yowsup.stacks import YowStack, YOWSUP_CORE_LAYERS
+from yowsup import env
 
 logging.basicConfig( level = logging.INFO )
 logger = logging.getLogger( __name__ )
@@ -43,14 +51,17 @@ try:
         config.credentials[ "password" ]
     )
 
-    stack = (
-        YowStackBuilder()
-        .pushDefaultLayers( True )
-        .push( ProxyLayer )
-        .build()
-    )
+    layers = (
+        ProxyLayer,
+        ( YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer )
+    ) + YOWSUP_CORE_LAYERS
 
-    stack.setCredentials( credentials )
+    stack = YowStack( layers )
+    stack.setProp( YowAuthenticationProtocolLayer.PROP_CREDENTIALS, credentials )
+    stack.setProp( YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0] )
+    stack.setProp( YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN )
+    stack.setProp( YowCoderLayer.PROP_RESOURCE, env.CURRENT_ENV.getResource() )
+
     stack.broadcastEvent( YowLayerEvent( YowNetworkLayer.EVENT_STATE_CONNECT ) )
 
     stack.loop()
